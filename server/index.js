5bf7c29601f63a49414c1de02d201df7dd2c34e6
eElec <adrishaditya@gmail.com>
2019-11-16 03:53:58 +0530

    Changes For Production

--- server/index.js
+++ server/index.js
@@ -1,26 +1,34 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
-const sql = require('mysql');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const sql = require("mysql");
+const path = require("path");
 
 //SETUP
 const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 //ROUTES
-const api = require('./routes/api');
-const auth = require('./routes/auth');
-app.use('/api', api);
-app.use('/auth', auth);
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
-});
+const api = require("./routes/api");
+const auth = require("./routes/auth");
+app.use("/api", api);
+app.use("/auth", auth);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/../build")));
+    app.get("*", (req, res) => {
+        res.sendfile(path.join((__dirname = "/../build/index.html")));
+    });
+} else {
+    app.use(express.static(path.join(__dirname, "/../public")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname + "/../public/index.html"));
+    });
+}
 
-
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Listening on Port: ${PORT}`);
-})
+});
