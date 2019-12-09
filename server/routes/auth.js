82cf68acb62cae538074ab27ac3c1a7d4e74d294
eElec <adrishaditya@gmail.com>
2019-12-09 20:33:14 +0530

    fix: disconnection issues

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,23 +5,36 @@ const sql = require('mysql');
 const withAuth = require('./withAuth');
 const secret = require('../secret');
 
-const database = sql.createConnection({
+var pool = sql.createPool({
+    connectionLimit: 5,
     host: "us-cdbr-iron-east-05.cleardb.net",
     user: "b17e2c57662428",
     password: "36c8dc80",
     database: "heroku_61079fc7f391c01"
 });
-database.connect();
+
+pool.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('Database connection was closed.')
+        }
+        else{
+            throw err;
+        }
+    }
+    if (connection) connection.release()
+    return
+})
 
 
 router.post('/', function(req, res) {
     const { uid, upass } = req.body;
 
-    database.query(`SELECT * FROM creds WHERE pass="${upass}" AND id="${uid}";`, (error, result, fields) => {
-        if (error) throw error;
+    pool.query(`SELECT * FROM creds WHERE pass="${upass}" AND id="${uid}";`, (error, result, fields) => {
+        if(error) { throw error; }
         let userInfo = {};
         if(result.length > 0){
-            database.query(`SELECT s_id, first_name, last_name  FROM Student WHERE s_id=${result[0].id};`, (err, result, fields)=>{
+            pool.query(`SELECT s_id, first_name, last_name  FROM Student WHERE s_id=${result[0].id};`, (err, result, fields)=>{
                 if (err) throw err;
                 if (result.length > 0) {
                     const payload = {...result[0]};
